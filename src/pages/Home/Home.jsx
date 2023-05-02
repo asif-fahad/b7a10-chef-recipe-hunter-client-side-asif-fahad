e0356a91e233cd43d607d055618fcbc6d725a522
asif-fahad <asif123fahad@gmail.com>
2023-05-02 20:00:17 +0600

    extra section one

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
         <div>
             <section className='bg-image '>
                 <div className='flex flex-col lg:flex-row justify-center justify-items-center'>
-
                     <div className=''>
                         <h1 className='text-2xl w-full lg:leading-tight sm:text-4xl lg:text-6xl lg:max-w-3xl text-white'>
                             One Step Closer
@@ -19,8 +18,29 @@ const Home = () => {
                             Explore many recipes with all the information you need.
                         </p>
                     </div>
-
                 </div >
+            </section>
+
+            <section className='mt-5'>
+                <div className='w-5/12 mx-auto'>
+                    <div className='text-center mb-2 text-3xl font-extrabold'>
+                        <h1>Special Features</h1>
+                    </div>
+                    <div className='flex gap-2'>
+                        <div className='border text-center w-72'>
+                            <h2 className='font-bold'>Food Nutritions</h2>
+                            <p>In the same way, the types that seem to us little clear now should become clear in the future.</p>
+                        </div>
+                        <div className='border text-center w-72'>
+                            <h2 className='font-bold'>Cooking Period</h2>
+                            <p>But the duis or him iriure pain in hendreit in vulputate wants to be molestie consequat, or that pain eu feugiat no facilisis.</p>
+                        </div>
+                        <div className='border text-center w-72'>
+                            <h2 className='font-bold'>Membership</h2>
+                            <p>As I have already seen, I will come to the smallest detail, who takes up the cover of our training program for the benefit of the players.</p>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     );
