38e14d86df72d4552d64cce894bbc34a97f2de4a
asif-fahad <asif123fahad@gmail.com>
2023-05-03 18:26:29 +0600

    spinner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,24 @@ import Chefs from '../Chefs/Chefs/Chefs';
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState();
 
+    if (loading) {
+        return <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span>
+        </div>
+    }
 
     useEffect((() => {
+        setLoading(true)
         fetch('https://b7a10-chef-recipe-hunter-server-side-asif-fahad-asif-fahad.vercel.app/chefs')
             .then(res => res.json())
             .then(data => setData(data))
+        setLoading(false);
     }), [])
 
     return (
