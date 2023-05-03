b547f0916415e465fe75cd46ca3085c69ddc10e9
asif-fahad <asif123fahad@gmail.com>
2023-05-03 12:28:49 +0600

    provider created

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault()
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+
     return (
         <div>
             <div>
@@ -11,20 +21,20 @@ const Login = () => {
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content">
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
-                                    <Link to='/registration' href="#" className="label-text-alt link link-hover">Don't have a account? Register now.</Link>
+                                    <Link to='/registration' className="label-text-alt link link-hover">Don't have a account? Register now.</Link>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
@@ -42,7 +52,7 @@ const Login = () => {
                                 src="https://i.ibb.co/g9f4P0S/github-btn.png"
                                 alt=""
                             />
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/pages/Login/Registration/Registration.jsx
+++ src/pages/Login/Registration/Registration.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
+
+    const handleRegister = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        console.log(email, password, name, photo);
+    }
+
+
     return (
         <div>
             <div>
                 <h1 className="text-5xl font-bold text-center">Register now!</h1>
             </div>
             <div className="hero min-h-screen bg-base-200">
-
                 <div className="hero-content ">
-
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <form onSubmit={handleRegister} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
@@ -28,15 +40,15 @@ const Registration = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <Link to='/login' className="label-text-alt link link-hover">Already have an account? Login now.</Link>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
@@ -54,7 +66,7 @@ const Registration = () => {
                                 src="https://i.ibb.co/g9f4P0S/github-btn.png"
                                 alt=""
                             />
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
