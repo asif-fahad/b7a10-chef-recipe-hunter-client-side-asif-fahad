b2cb2baa67980cf1a632f2934e25dfea05166dda
asif-fahad <asif123fahad@gmail.com>
2023-05-03 11:59:04 +0600

    login and register created

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     return (
         <div>
+            <div>
+                <h1 className="text-5xl font-bold text-center">Login now!</h1>
+            </div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <Link to='/registration' href="#" className="label-text-alt link link-hover">Don't have a account? Register now.</Link>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                            <img
 
+                                className="social-button"
+                                src="https://i.ibb.co/gSTHXZJ/google-btn.png"
+                                alt=""
+                            />
+                            <img
+
+                                className="social-button"
+                                src="https://i.ibb.co/g9f4P0S/github-btn.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Login/Registration/Registration.jsx
+++ src/pages/Login/Registration/Registration.jsx
@@ -3,7 +3,61 @@ import React from 'react';
 const Registration = () => {
     return (
         <div>
+            <div>
+                <h1 className="text-5xl font-bold text-center">Register now!</h1>
+            </div>
+            <div className="hero min-h-screen bg-base-200">
 
+                <div className="hero-content ">
+
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="photo url" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                            <img
+
+                                className="social-button"
+                                src="https://i.ibb.co/gSTHXZJ/google-btn.png"
+                                alt=""
+                            />
+                            <img
+
+                                className="social-button"
+                                src="https://i.ibb.co/g9f4P0S/github-btn.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
